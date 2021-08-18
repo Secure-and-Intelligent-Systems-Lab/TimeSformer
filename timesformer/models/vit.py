d8fea48b930eaeff7451860e3cc9cca3df841c18
Mohamed Mansour <70288518+mohamedmansour0@users.noreply.github.com>
2021-08-18 17:48:49 -0400

    Update vit.py
    Added save_inp_grad and get_inp_grad functions to the vision transformer class as in ViT_LRP model

--- timesformer/models/vit.py
+++ timesformer/models/vit.py
@@ -393,6 +393,13 @@ class VisionTransformer(nn.Module):
                       nn.init.constant_(m.temporal_fc.bias, 0)
                     i += 1
 
+    def save_inp_grad(self,grad):
+        self.inp_grad = grad
+
+    def get_inp_grad(self):
+        return self.inp_grad
+    
+    
     def _init_weights(self, m):
         if isinstance(m, nn.Linear):
             trunc_normal_(m.weight, std=.02)
