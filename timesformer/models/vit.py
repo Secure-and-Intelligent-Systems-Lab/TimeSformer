600425a725006d233d2f4f7b05244b13579ece86
Mohamed Mansour <70288518+mohamedmansour0@users.noreply.github.com>
2021-08-22 02:18:12 -0400

    Update vit.py
    fixed LayerNorm from nn.LayerNorm in Block function

--- timesformer/models/vit.py
+++ timesformer/models/vit.py
@@ -214,7 +214,7 @@ class Attention(nn.Module):
 class Block(nn.Module):
 
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0.1, norm_layer=nn.LayerNorm, attention_type='divided_space_time'):
+                 drop_path=0.1, attention_type='divided_space_time'):
         super().__init__()
         self.attention_type = attention_type
         assert(attention_type in ['divided_space_time', 'space_only','joint_space_time'])
@@ -232,7 +232,7 @@ class Block(nn.Module):
 
         ## drop path
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim, eps=1e-6)
+        self.norm2 = LayerNorm(dim, eps=1e-6)
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
         
